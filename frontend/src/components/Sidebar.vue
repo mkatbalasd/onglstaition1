89cd0a897ecdeaf20d567bd3679474990e4b7844
mkatbalasd <mkatbalasd@gmail.com>
2025-07-20 23:18:47 +0300

    Add Vue Router and basic layout

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,26 @@
+<template>
+  <aside class="bg-gray-100 dark:bg-gray-800 h-full p-4">
+    <nav class="space-y-2">
+      <RouterLink to="/" class="flex items-center space-x-2">
+        <Home class="w-4 h-4" />
+        <span>Dashboard</span>
+      </RouterLink>
+      <RouterLink to="/driver-cards" class="flex items-center space-x-2">
+        <IdCard class="w-4 h-4" />
+        <span>Driver Cards</span>
+      </RouterLink>
+      <RouterLink to="/operating-cards" class="flex items-center space-x-2">
+        <SquareKanban class="w-4 h-4" />
+        <span>Operating Cards</span>
+      </RouterLink>
+    </nav>
+  </aside>
+</template>
+
+<script setup>
+import { Home, IdCard, SquareKanban } from 'lucide-vue-next'
+import { RouterLink } from 'vue-router'
+</script>
+
+<style scoped>
+</style>
