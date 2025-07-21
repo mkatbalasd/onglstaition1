503c587a92484fbddd784ec05efb461fd3d6ed3c
mkatbalasd <mkatbalasd@gmail.com>
2025-07-21 18:21:31 +0300

    Add reusable layout components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,18 @@
 <script setup>
+import Header from './components/Header.vue'
+import Sidebar from './components/Sidebar.vue'
 </script>
 
 <template>
-  <RouterView />
+  <div class="min-h-screen flex">
+    <Sidebar class="w-64" />
+    <div class="flex-1 flex flex-col">
+      <Header />
+      <main class="flex-1 p-4 bg-gray-50 dark:bg-gray-900">
+        <RouterView />
+      </main>
+    </div>
+  </div>
 </template>
 
 <style scoped>
--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="bg-white dark:bg-gray-900 rounded shadow p-4 flex items-start space-x-3 rtl:space-x-reverse"
+    class="bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded shadow p-4 flex items-start space-x-3 rtl:space-x-reverse"
   >
-    <component v-if="icon" :is="icon" class="w-5 h-5 text-gray-500 dark:text-gray-400" />
+    <component v-if="icon" :is="icon" class="w-5 h-5 text-blue-600 dark:text-blue-400" />
     <div class="flex-1">
       <slot />
     </div>
