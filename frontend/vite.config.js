ba0e0ad6522bdb82225fc0f2be982aeaa358bfe7
mkatbalasd <mkatbalasd@gmail.com>
2025-07-21 22:28:10 +0300

    fix(frontend): set default base path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/nagl/app/',
+  base: '/nagl/app/',
   plugins: [
     vue(),
     vueDevTools(),
