f3d38cf272a109148ed292dc2f96d3806ece244a
mkatbalasd <mkatbalasd@gmail.com>
2025-07-21 03:48:10 +0300

    Configure Vue router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -12,34 +12,9 @@ const routes = [
     component: () => import('../views/DriverCards.vue')
   },
   {
-    path: '/driver-cards/new',
-    name: 'DriverCardsNew',
-    component: () => import('../views/DriverCardsNew.vue')
-  },
-  {
-    path: '/driver-cards/new/:facilityId/driver',
-    name: 'DriverCardsDriver',
-    component: () => import('../views/DriverCardsDriver.vue')
-  },
-  {
-    path: '/driver-cards/:id',
-    name: 'DriverCardsForm',
-    component: () => import('../views/DriverCardsForm.vue')
-  },
-  {
-    path: '/driver-cards/:id/print',
-    name: 'DriverCardsPrint',
-    component: () => import('../views/DriverCardsPrint.vue')
-  },
-  {
-    path: '/operating-cards',
-    name: 'OperatingCards',
+    path: '/cards',
+    name: 'Cards',
     component: () => import('../views/OperatingCards.vue')
-  },
-  {
-    path: '/operating-cards/new',
-    name: 'OperatingCardsNew',
-    component: () => import('../views/OperatingCardsNew.vue')
   },
   {
     path: '/facilities',
@@ -47,29 +22,14 @@ const routes = [
     component: () => import('../views/Facilities.vue')
   },
   {
-    path: '/facilities/new',
-    name: 'FacilitiesNew',
-    component: () => import('../views/FacilitiesNew.vue')
-  },
-  {
     path: '/drivers',
     name: 'Drivers',
     component: () => import('../views/Drivers.vue')
   },
   {
-    path: '/drivers/new',
-    name: 'DriversNew',
-    component: () => import('../views/DriversNew.vue')
-  },
-  {
     path: '/vehicles',
     name: 'Vehicles',
     component: () => import('../views/Vehicles.vue')
-  },
-  {
-    path: '/vehicles/new',
-    name: 'VehiclesNew',
-    component: () => import('../views/VehiclesNew.vue')
   }
 ]
 
