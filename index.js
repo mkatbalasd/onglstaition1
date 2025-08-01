77827fe1179283f5f8e8ff24486d11604446ab68
mkatbalasd <mkatbalasd@gmail.com>
2025-08-01 23:38:58 +0300

    Update index.js
    Co-authored-by: qodo-merge-pro[bot] <151058649+qodo-merge-pro[bot]@users.noreply.github.com>

--- index.js
+++ index.js
@@ -32,6 +32,9 @@ app.use('/nagl', cardsRouter);
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err);
+  if (res.headersSent) {
+    return next(err);
+  }
   res.status(500).send('Server Error');
 });
 
