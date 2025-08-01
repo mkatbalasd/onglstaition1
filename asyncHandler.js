5ec2bed67ca18ee2824c87fa424502d1b8f08bd0
mkatbalasd <mkatbalasd@gmail.com>
2025-08-01 23:40:29 +0300

    Update asyncHandler.js
    Co-authored-by: qodo-merge-pro[bot] <151058649+qodo-merge-pro[bot]@users.noreply.github.com>

--- asyncHandler.js
+++ asyncHandler.js
@@ -1,4 +1,7 @@
 module.exports = function asyncHandler(fn) {
+  if (typeof fn !== 'function') {
+    throw new TypeError('asyncHandler expects a function');
+  }
   return function wrapped(req, res, next) {
     Promise.resolve(fn(req, res, next)).catch(next);
   };
